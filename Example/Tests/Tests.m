b25e48b6fb0c802f45e03207ca36bde179145bc2
Tobias Sundstrand <tobias.sundstrand@gmail.com>
2015-01-25 23:22:27 +0100

    Updated Specta version

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -17,10 +17,6 @@ describe(@"these will fail", ^{
     it(@"can read", ^{
         expect(@"number").to.equal(@"string");
     });
-    
-    it(@"will wait and fail", ^AsyncBlock {
-        
-    });
 });
 
 describe(@"these will pass", ^{
@@ -31,12 +27,6 @@ describe(@"these will pass", ^{
     
     it(@"can read", ^{
         expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^AsyncBlock {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-            done();
-        });
     });
 });
 
