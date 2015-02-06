1708a2138b2b2e82fca5fd5f33de822a3676c033
Tobias Sundstrand <tobias.sundstrand@gmail.com>
2015-02-06 14:15:35 +0100

    Addad lib header

---
+++ Pod/Classes/Realm+Rest.h
@@ -0,0 +1,20 @@
+//
+//  Realm+Rest.h
+//  Realm-Rest
+//
+//  Created by Tobias Sundstrand on 15-02-06.
+//
+//
+
+#ifndef Pods_Realm_Rest____FILEEXTENSION___
+#define Pods_Realm_Rest____FILEEXTENSION___
+
+#import <Realm-Rest/RLMObject+Rest.h>
+#import <Realm-Rest/RLMRealm+Rest.h>
+#import <Realm-Rest/RestOrchestrator.h>
+#import <Realm-Rest/RestPathFinder.h>
+#import <Realm-Rest/RestRequestBuilder.h>
+#import <Realm-Rest/RestRequestQueue.h>
+#import <Realm-Rest/RestNotifier.h>
+
+#endif
