307c10ecb129a509346a2b75d8d9203a4784e29e
Tobias Sundstrand <tobias.sundstrand@gmail.com>
2015-08-13 17:15:07 +0200

    Update version to 2.1.0

--- Example/Pods/Target Support Files/Pods-Realm-Rest/Pods-Realm-Rest-environment.h
+++ Example/Pods/Target Support Files/Pods-Realm-Rest/Pods-Realm-Rest-environment.h
@@ -105,8 +105,8 @@
 // Realm-Rest
 #define COCOAPODS_POD_AVAILABLE_Realm_Rest
 #define COCOAPODS_VERSION_MAJOR_Realm_Rest 2
-#define COCOAPODS_VERSION_MINOR_Realm_Rest 0
-#define COCOAPODS_VERSION_PATCH_Realm_Rest 9
+#define COCOAPODS_VERSION_MINOR_Realm_Rest 1
+#define COCOAPODS_VERSION_PATCH_Realm_Rest 0
 
 // Realm/Headers
 #define COCOAPODS_POD_AVAILABLE_Realm_Headers
